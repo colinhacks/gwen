d177a38751b685c09f7a59a79b7f43a87cf71ace
Colin McDonnell <colinmcd@alum.mit.edu>
2020-08-07 17:34:48 -0700

    update

--- src/external.ts
+++ src/external.ts
@@ -1,8 +1,9 @@
 import { Gwen } from './Gwen';
+import { extractCritical } from 'emotion-server';
 
 export const gwen = () =>
   new Gwen({
     sansFont: '',
   });
 
-export { Gwen };
+export { Gwen, extractCritical };
